2ee04c3ddadb963f566f67d73b3dcc11addb7fc3
quentin <brunetq38@gmail.com>
2023-02-19 06:23:54 +0100

    add form types for new user

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -7,17 +7,20 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Gregwar\CaptchaBundle\Type\CaptchaType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 
 class UserType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email')
+            ->add('email', EmailType::class)
             ->add('roles')
-            ->add('password')
-            ->add('nom')
-            ->add('prenom')
+            ->add('password', PasswordType::class)
+            ->add('nom', TextType::class)
+            ->add('prenom', TextType::class)
             ->add('captcha', CaptchaType::class);
     }
 
